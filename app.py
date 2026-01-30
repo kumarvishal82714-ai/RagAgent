1f4d0263c4d4259e6a3bf902aaed0f15b028c9c2
Vishal Kumar <your-email@gmail.com>
2026-01-30 13:01:32 +0530

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-
+#try
 # 1. Load data
 loader = TextLoader("data.txt")
 documents = loader.load()
